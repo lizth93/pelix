e77e915d12bddc64ca66d5f8c16c799b7addbd1d
lizth93 <lizth93@hotmail.com>
2022-08-19 10:36:24 -0500

    Carrousel

--- src/icons/add.js
+++ src/icons/add.js
@@ -1,4 +1,4 @@
-const AddIcon = () => {
+const AddIcon = (props) => {
   return (
     <svg
       width="24"
@@ -7,6 +7,7 @@ const AddIcon = () => {
       fillRule="evenodd"
       clipRule="evenodd"
       className="add-icon"
+      onClick={props.onClick}
     >
       <path d="M11.5 0c6.347 0 11.5 5.153 11.5 11.5s-5.153 11.5-11.5 11.5-11.5-5.153-11.5-11.5 5.153-11.5 11.5-11.5zm0 1c5.795 0 10.5 4.705 10.5 10.5s-4.705 10.5-10.5 10.5-10.5-4.705-10.5-10.5 4.705-10.5 10.5-10.5zm.5 10h6v1h-6v6h-1v-6h-6v-1h6v-6h1v6z" />
     </svg>
