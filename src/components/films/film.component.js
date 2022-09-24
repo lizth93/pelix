87a4456d3d9a3ce4144b5af70064254a6ad385dc
lizth93 <lizth93@hotmail.com>
2022-09-24 18:48:47 -0500

    Fixed bug at load the detail page

--- src/components/films/film.component.js
+++ src/components/films/film.component.js
@@ -13,9 +13,14 @@ const Film = (props) => {
     props.onClickFilm(props.collection.id);
   };
   return (
-    <div className={className} onClick={handleClick}>
+    <div className={className}>
       <figure className="collection__fig">
-        <img src={props.src} alt={props.title} className="collection__img" />
+        <img
+          src={props.src}
+          alt={props.title}
+          className="collection__img"
+          onClick={handleClick}
+        />
       </figure>
       <div className="collection__title">
         <h1>
