d80f32dd5b935293ec84bcaea079a67346efb379
lizth93 <lizth93@hotmail.com>
2022-09-29 11:10:32 -0500

    Added media queries for responsive design

--- src/core/search/results/index.js
+++ src/core/search/results/index.js
@@ -10,11 +10,49 @@ export default styled(SearchResults)`
     margin-bottom: 3rem;
     display: flex;
     gap: 3rem;
+
+    @media (max-width: 50em) {
+      //800px
+      flex-wrap: wrap;
+      justify-content: center;
+    }
   }
   .list-group {
     min-width: 20rem;
     font-size: 1.5rem;
+    --bs-list-group-bg: #212529;
+    --bs-list-group-color: #fff;
+    --bs-primary-rgb: 11, 114, 133;
+    --bs-list-group-border-color: rgba(11, 114, 133, 0.5);
+
     cursor: pointer;
+
+    @media (max-width: 50em) {
+      //800px
+      width: 100%;
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      border-bottom-left-radius: 0.5rem;
+      border-radius: 0;
+      justify-content: center;
+    }
+
+    &-item:first-child {
+      border-top-left-radius: 0.5rem;
+      border-bottom-left-radius: 0.5rem;
+    }
+
+    &-item:last-child {
+      border-top-right-radius: 0.5rem;
+      border-bottom-right-radius: 0.5rem;
+    }
+  }
+
+  .fw-bold:first-child {
+    @media (max-width: 50em) {
+      //800px
+      padding-right: 1rem;
+    }
   }
   .section-results {
     margin: 0 auto;
